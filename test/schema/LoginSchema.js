e3ac47b86ac7ad04bccdb720eff24920b581ecd3
Aravind <aravind.1si13cs043@gmail.com>
2019-04-05 17:16:24 +0530

    Adding test for the Login Flow and schema validation file

---
+++ test/schema/LoginSchema.js
@@ -0,0 +1,55 @@
+/** This ia a example login schema for the login having userid, isverfied status, authtoken and refreshtoken
+        here we are validating the id type as int, isverified status as bool auth and ref token as string **/
+
+exports.loginSchema =
+    {
+        "type": "object",
+        "properties": {
+            "status": {
+                "type": "string"
+            },
+            "data": {
+                "type": "object",
+                "properties": {
+                    "user": {
+                        "type": "object",
+                        "properties": {
+                            "id": {
+                                "type": "integer"
+                            },
+                            "isVerified": {
+                                "type": "boolean"
+                            },
+                            "token": {
+                                "type": "object",
+                                "properties": {
+                                    "authToken": {
+                                        "type": "string"
+                                    },
+                                    "refreshToken": {
+                                        "type": "string"
+                                    }
+                                },
+                                "required": [
+                                    "authToken",
+                                    "refreshToken"
+                                ]
+                            }
+                        },
+                        "required": [
+                            "id",
+                            "isVerified",
+                            "token"
+                        ]
+                    }
+                },
+                "required": [
+                    "user"
+                ]
+            }
+        },
+        "required": [
+            "status",
+            "data"
+        ]
+    };
---
+++ test/specs/common/Login.js
@@ -0,0 +1,34 @@
+const mocha = require('mocha');
+const supertest = require('supertest');
+const LoginActions = require('../../actions/common/LoginActions.js');
+const data = require('../../testData/LoginUserInfo').UserLoginData;
+const statusCode = require('../../../StatusCode').StatusCode;
+const statusMessage = require('../../testData/DefaultHeaders').statusMessages;
+const schemaValidation = require('../../utils/SchemaUtils');
+const loginSchema = require('../../schema/LoginSchema').loginSchema;
+
+let getauthToken = '';
+
+describe("Login Api", () => {
+
+    it('Login check : Valid User Credentials', async function () {
+        let res = await LoginActions.loginToUser(data.VALID_USER_CREDENTIALS.username, data.VALID_USER_CREDENTIALS.password);
+        console.log('Validating the Response status code check');
+        res.status.should.equal(statusCode.OK);
+        console.log('Validating the Response Status Message');
+        res.body.status.should.equal(statusMessage.SUCCESS);
+        console.log('Get the Generated Status code and storing the getauthToken variable');
+        getauthToken = res.body.data.user.token.authToken;
+        console.log("You are just printing the generated Auth Token:", getauthToken);
+        schemaValidation.ValidateJsonSchema(loginSchema, res.body);
+    });
+
+    it('Login check ( Negative ) : Invalid Credentials', async function () {
+        let res = await LoginActions.loginToUser(data.INVALID_USER_CREDENTIALS.username, data.INVALID_USER_CREDENTIALS.password);
+        console.log('Validating the Response status code check');
+        res.status.should.equal(statusCode.UNAUTHORIZED);
+        console.log('Validating the Response Status Message');
+        res.body.status.should.equal(statusMessage.FAILURE);
+    }); 
+
+});
